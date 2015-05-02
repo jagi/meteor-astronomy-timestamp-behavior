aba10bbf5e6901ccd3de07a6e284983ebb112e7b
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-05-02 15:35:30 +0200

    Initial commit

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  summary: 'Timestamp behavior for Meteor Astronomy',
+  version: '0.1.0',
+  name: 'jagi:astronomy-timestamp-behavior',
+  git: 'https://github.com/jagi/meteor-astronomy-timestamp-behavior.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+
+  api.use('jagi:astronomy-behaviors@0.2.0');
+  api.use('underscore');
+
+  // Behavior.
+  api.addFiles('lib/behavior/events.js', ['client', 'server']);
+  api.addFiles('lib/behavior/behavior.js', ['client', 'server']);
+});

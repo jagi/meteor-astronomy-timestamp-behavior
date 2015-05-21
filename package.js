0c6ce5e73df74340182b0b0e3f1214c945226bff
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-05-21 23:12:09 +0200

    Use addbehavior event

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Timestamp behavior for Meteor Astronomy',
-  version: '0.4.0',
+  version: '0.4.1',
   name: 'jagi:astronomy-timestamp-behavior',
   git: 'https://github.com/jagi/meteor-astronomy-timestamp-behavior.git'
 });
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy-behaviors@0.5.0');
+  api.use('jagi:astronomy-behaviors@0.5.1');
   api.use('underscore');
 
   // Behavior.
